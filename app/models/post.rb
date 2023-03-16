b54035fd4d5e5af752f2a54b8e064f980e9597f7
jeff ndungu <ndungujeff.ke@gmail.com>
2023-03-16 16:10:24 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :category, include:["Fiction", "Non-Fiction"]
+    validates :content, length: {minimum: 100}
 end
